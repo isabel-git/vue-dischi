800eeb9d5e6aebdfdd4b463278b068af52cd4cba
sourceCodeISA <isabel.gogov1@libero.it>
2020-12-01 15:19:52 +0100

    versione grezza

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@
 // https://flynn.boolean.careers/exercises/api/array/music
 // L’api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
 
-
+const apiMusic = "https://flynn.boolean.careers/exercises/api/array/music"
 
 var app = new Vue({
   el: '#app',
   data: {
-    
+    album: [{
+
+    }]
+
   },
- 
-  methods: {
-    
-    }
+  mounted: function () {
+    axios.get(apiMusic)
+      .then(risposta => {
+
+        this.album = risposta.data.response;
+        console.log(risposta.data.response);
+      });
+
+  }
 
 });
