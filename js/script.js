e23ce8a0031f8efa42cf1263a771a02ba001f4d1
sourceCodeISA <isabel.gogov1@libero.it>
2020-12-01 14:22:34 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Facciamo una chiamata ajax all’api di boolean al seguente indirizzo.
+// https://flynn.boolean.careers/exercises/api/array/music
+// L’api ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
+
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    
+  },
+ 
+  methods: {
+    
+    }
+
+});
